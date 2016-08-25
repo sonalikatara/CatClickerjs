37ffca33f272ce7f43410f8ccbff9f47879188bf
sonali <sonalikatara@gmail.com>
2016-08-24 22:55:42 -0700

    now make this work for two or more cats .. jquery dom traversing not working yet

--- src/main.js
+++ src/main.js
@@ -13,11 +13,32 @@ $(document).ready(function(){
          ]
     };
 
-        $('#catImg').click(function(e){
-        // the image has been clicked... increase the click count
-        var count = $('#clickCount').text();
-        count++;
-        $('#clickCount').text(count);
-        //alert('clickCount ' + count);
-    });
+         cats.display = function(){
+         var allCats = $('#AllCats');
+         if (cats.cats.length !=0 ){
+           for (cat in cats.cats)
+           {
+             var showClickCount ="<div id = 'showClickCount' class='lead text-center'>Your Click Count Score is <span id='clickCount'>1</span>";
+
+             var catImage = "<figure id='showCats' class='text-center'><img id='catImg' class='img-polaroid' src='" + cats.cats[cat].image +"' onClick='catClicked(this)' alt='cat' ><figcaption> Click " + cats.cats[cat].name + " ! </figcaption></figure></div>";
+             console.log(catImage);
+             allCats.append(showClickCount + catImage);
+           }
+         };
+    };
+
+
+
+    cats.display();
 });
+
+function catClicked(e){
+        // the image has been clicked... increase the click count
+        var thisCat = e.id;
+
+
+        var count = $(thisCat).prevUntil("spam").text();
+        var clicks = parseInt(count) + 1;
+        $(thisCat).parent().parent().children('clickCount').text(clicks);
+
+    };

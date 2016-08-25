5c8340dbb54366bd1a94940effe102d9f4d769c2
sonali <sonalikatara@gmail.com>
2016-08-24 23:01:58 -0700

    remove spaces to make the code clean

--- src/main.js
+++ src/main.js
@@ -19,24 +19,18 @@ $(document).ready(function(){
            for (cat in cats.cats)
            {
              var showClickCount ="<div id = 'showClickCount' class='lead text-center'>Your Click Count Score is <span id='clickCount'>1</span>";
-
              var catImage = "<figure id='showCats' class='text-center'><img id='catImg' class='img-polaroid' src='" + cats.cats[cat].image +"' onClick='catClicked(this)' alt='cat' ><figcaption> Click " + cats.cats[cat].name + " ! </figcaption></figure></div>";
              console.log(catImage);
              allCats.append(showClickCount + catImage);
            }
          };
     };
-
-
-
     cats.display();
 });
 
 function catClicked(e){
         // the image has been clicked... increase the click count
         var thisCat = e.id;
-
-
         var count = $(thisCat).prevUntil("spam").text();
         var clicks = parseInt(count) + 1;
         $(thisCat).parent().parent().children('clickCount').text(clicks);
